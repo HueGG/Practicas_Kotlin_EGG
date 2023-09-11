cba2e9576e6fc7a05036981417b42ee5ad7fee1d
HueGG <66105403+HueGG@users.noreply.github.com>
2023-09-10 21:44:03 -0600

    Add files via upload
    Hola UPIICSA

---
+++ Main.kt
@@ -0,0 +1,12 @@
+/**
+ * UPIICSA
+ * Progrmación Móvil
+ * Grupo 6NM61
+ * Práctica 01 Alternativa Online para Testeo Rapido
+ * Eduardo
+ */
+fun main(args: Array<String>) {
+    print("Hello, UPIICSA")
+    println(" Bienvenido")
+    println("Programación movil con Kotlin")
+}
